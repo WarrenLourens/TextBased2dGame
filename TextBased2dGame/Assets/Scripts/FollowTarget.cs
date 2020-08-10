2cb95f5e93096730e59d97f0d4ef528be08757d1
WarrenLourens <Warren.Lourens23@gmail.com>
2020-08-11 01:13:54 +1200

    The first push Shadows of Ronnin

---
+++ TextBased2dGame/Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    // what we are following
+    public Transform player;
+    // zeros out the velocity
+    Vector3 veocity = Vector3.zero;
+    // time to follow player
+    public float smoothTime = .15f;
+    //enable and set the maximum y value
+    public bool YMaxEnabled = false;
+    public float YMaxValue = 0;
+    // enable and set the minimum y value
+    public bool YMinEnabled = false;
+    public float YMinValue = 0;
+    // enable and set the maximum x value
+    public bool XMaxEnabled = false;
+    public float XMaxValue = 0;
+    // enable and set the minimum x value
+    public bool XMinEnabled = false;
+    public float XMinValue = 0;
+    void FixedUpdate()
+    {//player position
+        Vector3 playerPosition = player.position;
+
+        // Vertical
+        if (YMinEnabled && YMaxEnabled)
+        {
+            playerPosition.y = Mathf.Clamp(player.position.y, YMinValue, YMaxValue);
+        }
+        else if (YMinEnabled)
+        {
+            playerPosition.y = Mathf.Clamp(player.position.y, YMinValue, player.position.y);
+        }
+        else if (YMaxEnabled)
+        {
+            playerPosition.y = Mathf.Clamp(player.position.y, player.position.y, YMaxValue);
+        }
+        // Horizontal 
+        if (XMinEnabled && XMaxEnabled)
+        {
+            playerPosition.x = Mathf.Clamp(player.position.x, XMinValue, XMaxValue);
+        }
+        else if (XMinEnabled)
+        {
+            playerPosition.x = Mathf.Clamp(player.position.x, XMinValue, player.position.x);
+        }
+        else if (XMaxEnabled)
+        {
+            playerPosition.x = Mathf.Clamp(player.position.x, player.position.x, XMaxValue);
+        }
+            // aligns the camera and the target z position
+            playerPosition.z = transform.position.z;
+
+        transform.position = Vector3.SmoothDamp(transform.position,playerPosition, ref veocity,smoothTime);
+
+    }
+}
---
+++ TextBased2dGame/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private bool Attack;
+    private Rigidbody2D PlayerRigidBody;
+    private bool facingRight;
+    private Animator myAnimator;
+    [SerializeField]
+    private float MoveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        facingRight = true;
+        PlayerRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+       
+        
+    }
+    private void Update()
+    {
+        HandleInput();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        HandleMovement(horizontal);
+        Flip(horizontal);
+        HandleAttacks();
+        ResetValues();
+    }
+    private void HandleMovement(float horizontal)
+    {
+        if (!this.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack")) 
+        {
+            PlayerRigidBody.velocity = new Vector2(horizontal*MoveSpeed, PlayerRigidBody.velocity.y);
+        }
+    
+        myAnimator.SetFloat("Speed", Mathf.Abs(horizontal));
+    }
+
+    private void HandleAttacks() 
+    {
+        if (Attack && !this.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack")) 
+        {
+            myAnimator.SetTrigger("Attack");
+            PlayerRigidBody.velocity = Vector2.zero;
+        }
+    }
+    private void HandleInput() 
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift)) 
+        {
+            Attack = true;
+        }
+    }
+
+    private void Flip(float movement) 
+    {
+        if (movement > 0 && !facingRight || movement < 0 && facingRight) 
+        {
+            facingRight = !facingRight;
+            Vector2 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+
+    private void ResetValues() 
+    {
+        Attack = false;
+    }
+}
